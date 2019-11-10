8040ac42e73508e81e1fc487b4ebb5fa8ff120f0
ehinson <erinqhinson@gmail.com>
2019-11-10 11:55:40 -0700

    error handling: add log file and email logging

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # To start
 
 `source venv/bin/activate`
+
 `flask run`
+
+[SQLAlchemy documentation](https://flask-sqlalchemy.palletsprojects.com/en/2.x/api/#flask_sqlalchemy.SQLAlchemy)
--- app/models.py
+++ app/models.py
@@ -28,7 +28,7 @@ class User(UserMixin, db.Model):
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title =  db.Column(db.String(120))
-  body =  db.Column(db.String(1000))
+  body =  db.Column(db.Text)
   timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
