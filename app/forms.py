0ebd0c73eaa2b5d0dcb504d4bda9f46b43b984d2
ehinson <erinqhinson@gmail.com>
2019-11-24 19:31:02 -0700

    password reset flow works here

--- app/forms.py
+++ app/forms.py
@@ -51,3 +51,9 @@ class PostForm(FlaskForm):
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
