e016b6e4c86c3d9b6c56db0af57e7bc63adf67ba
ehinson <erinqhinson@gmail.com>
2019-11-29 15:55:43 -0700

    Register Auth Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,12 @@ babel = Babel(app)
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
 
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp)
+
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
+
 if not app.debug:
   if app.config['MAIL_SERVER']:
     auth = None
