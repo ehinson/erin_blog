80efcaf6477d6b9087f9e5f5290e1affe648da27
ehinson <erinqhinson@gmail.com>
2019-11-30 18:46:18 -0700

    create_app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,55 +10,65 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
-
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page.')
-mail = Mail(app)
-moment = Moment(app)
-babel = Babel(app)
+mail = Mail()
+moment = Moment()
+babel = Babel()
 
-from app.errors import bp as errors_bp
-app.register_blueprint(errors_bp)
+def create_app(config_class=Config):
+  app = Flask(__name__)
+  app.config.from_object(config_class)
 
-from app.auth import bp as auth_bp
-app.register_blueprint(auth_bp)
+  db.init_app(app)
+  migrate.init_app(app, db)
+  login.init_app(app)
+  mail.init_app(app)
+  moment.init_app(app)
+  babel.init_app(app)
 
-from app.main import bp as main_bp
-app.register_blueprint(main_bp)
+  from app.errors import bp as errors_bp
+  app.register_blueprint(errors_bp)
 
-if not app.debug:
-  if app.config['MAIL_SERVER']:
-    auth = None
-    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-      auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-    secure = None
-    if app.config['MAIL_USE_TLS']:
-      secure = ()
-    mail_handler = SMTPHandler(
-      mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-      fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-      toaddrs=app.config['ADMINS'], subject='Blog Failure',
-      credentials=auth, secure=secure)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
-  if not os.path.exists('logs'):
-    os.mkdir('logs')
-  file_handler = RotatingFileHandler('logs/blog.log', maxBytes=10240, backupCount=10)
-  file_handler.setFormatter(logging.Formatter(
-    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-  file_handler.setLevel(logging.INFO)
-  app.logger.addHandler(file_handler)
+  from app.auth import bp as auth_bp
+  app.register_blueprint(auth_bp)
 
-  app.logger.setLevel(logging.INFO)
-  app.logger.info('Blog startup')
+  from app.main import bp as main_bp
+  app.register_blueprint(main_bp)
+
+  if not app.debug and not app.testing:
+    if app.config['MAIL_SERVER']:
+      auth = None
+      if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+      secure = None
+      if app.config['MAIL_USE_TLS']:
+        secure = ()
+      mail_handler = SMTPHandler(
+        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+        toaddrs=app.config['ADMINS'], subject='Blog Failure',
+        credentials=auth, secure=secure)
+      mail_handler.setLevel(logging.ERROR)
+      app.logger.addHandler(mail_handler)
+    if not os.path.exists('logs'):
+      os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/blog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+      '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Blog startup')
+
+  return app
 
 @babel.localeselector
 def getLocale():
-  return request.accept_languages.best_match(app.config['LANGUAGES'])
+  return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 
 from app import models
