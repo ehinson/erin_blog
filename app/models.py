e008b5b29749b424a13576849af2fe9392814379
ehinson <erinqhinson@gmail.com>
2020-05-03 15:37:47 -0600

    Notification migration and model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from time import time
+import json
 import jwt
 from flask import current_app
 from app import db, login
@@ -93,6 +94,7 @@ class User(UserMixin, db.Model):
                                         foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref="user", lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -165,6 +167,17 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
 
 
 @login.user_loader
