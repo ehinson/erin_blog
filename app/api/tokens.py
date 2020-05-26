5032237b3f307bb0f9d357d0d3d154f329fa8d4a
Erin Hinson <ehinson@treetopllc.com>
2020-05-25 19:54:48 -0600

    update last_seen to a more reasonable time

--- app/api/tokens.py
+++ app/api/tokens.py
@@ -2,11 +2,15 @@ from flask import jsonify
 from app import db
 from app.api import bp
 from app.api.auth import basic_auth, token_auth
+from datetime import datetime
+
 
 @bp.route('/tokens', methods=['POST'])
 @basic_auth.login_required
 def get_token():
     token = basic_auth.current_user().get_token()
+    if token:
+        basic_auth.current_user().last_seen = datetime.utcnow()
     db.session.commit()
     return jsonify({'token': token, "current_user": basic_auth.current_user().to_dict() })
 
